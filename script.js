e6a3a9dbd8ae41f5d0e8e124bd6cb3efd31bd3ff
Yotsaphon Liupolvanish <yotsaphon.liu@thinkerfint.com>
2024-10-03 12:13:20 +0700

    fix css for dropdown

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("active");
 }
+
+function toggleSubMenu() {
+  const subMenu = document.getElementById("dropdown-submenu");
+  subMenu.classList.toggle("active");
+}
